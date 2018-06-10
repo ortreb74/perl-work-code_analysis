fb0a34b7eff01403d6a0fa707bcfe5f142fd8523
pierre donzel <p.donzel-ext@lefebvre-sarrut.eu>
2018-06-10 16:31:27 +0200

    RSOA était en double et avait pris la place de AHL

--- analyzeHulkLiv/AHL.pl
+++ analyzeHulkLiv/AHL.pl
@@ -5,30 +5,53 @@ use warnings;
 
 # egrep "^\w+:" bin/hulkliv
 
-my $inputFileName = "StepOneAction.java";
+my $inputFileName = "hulkliv";
 
-my $outputFileName = "out.txt";
+my $defaultOutputFileName = "out/aside";
+my $commentsOutputFileName = "out/comments";
+my $loaderOutputFileName = "out/loader";
 
 open (my $inputFile, '<', $inputFileName) or die "Could not open file $inputFileName";
-open (my $outputFile, '>', $outputFileName) or die "Could not open file $outputFileName";
+open (my $outputAsideFile, '>', $defaultOutputFileName) or die "Could not open file $defaultOutputFileName";
+open (my $outputCommentsFile, '>', $commentsOutputFileName) or die "Could not open file $defaultOutputFileName";
+open (my $outputLoaderFile, '>', $loaderOutputFileName) or die "Could not open file $loaderOutputFileName";
+
+my $outputFile = $outputLoaderFile;
 
 # il y a un premier bloc, jusqu'au premier mot clef "set"
 # puis il y a un deuxième bloc jusqu'au premier arg et qui se termine à end 
 
 my $step = 1;
 my @words;
-my $next = 0;
 
 while (my $line = <$inputFile>) {	
-	if ($line =~ /setDestination/) {
-		print $outputFile $line;
-		$next =  !($line =~ /\)/);			
-	} elsif ($next) {
-		print $outputFile $line;
-		$next =  !($line =~ /\)/);			
+	if ($line =~ /^\s*#/) {
+		print $outputCommentsFile $line;
+		next;
+	} 
+	
+	@words = split (" ", $line);
+	
+	if ($step == 1 && grep( /^set$/, @words)) {		
+		$step = 2;
+		$outputFile = $outputAsideFile;
 	}
 	
+	if ($step == 2 && grep( /^\$arg$/, @words)) {
+		$step ++;
+		$outputFile = $outputAsideFile;
+	}	
+	
+	if ($step == 3 && $line =~  /^end$/) {
+		$step ++;
+		$outputFile = $outputAsideFile;
+	}	
+	
+	print $outputFile $line;	
 }
 
 close ($inputFile);
 close ($outputFile);
+close ($outputCommentsFile);
+close ($outputAsideFile);
+close ($outputLoaderFile);
